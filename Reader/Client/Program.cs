1b63b51758a5782101908ae422c21a8aeeea89a8
Josh Silverman <jsilverman@schoolsfirstfcu.org>
2024-05-01 09:58:54 -0700

    initial

---
+++ Reader/Client/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Reader.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("Reader.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Reader.ServerAPI"));
+
+await builder.Build().RunAsync();
